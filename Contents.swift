b965f796c645c705996f0a50751dddf3ea376016
Saleh <saleh.binessa@gmail.com>
2024-03-19 23:11:07 +0300

    first commit

---
+++ Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+// 1) Grading System 🤼
+
+let grade = "A"
+
+switch grade {
+case "A":
+    print("Excellent")
+case "B":
+    print("Good")
+case "C":
+    print("Average")
+case "D":
+    print("Below Average")
+case "F":
+    print("Poor")
+default:
+    print("Invalid Grade")
+}
+
+// 2) Traffic Lights 🚦
+
+let trafficLight = "green"
+
+switch trafficLight {
+case "red":
+    print("Stop")
+case "yellow":
+    print("Get Ready")
+case "green":
+    print("Go")
+default:
+    print("Invalid Traffic Light Color")
+}
